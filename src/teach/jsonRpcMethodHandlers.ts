f4a63df42a34ec04170e98ec24fe64635fba724d
bctran <bao.cong.tran@mgm-tp.com>
2023-10-22 21:21:08 +0700

    Update

--- src/teach/jsonRpcMethodHandlers.ts
+++ src/teach/jsonRpcMethodHandlers.ts
@@ -2,7 +2,16 @@ import "scope-extensions-js";
 import { JSONRPCServer } from "json-rpc-2.0";
 
 export default new JSONRPCServer().apply(function () {
+  this.addMethod("createLesson", createLesson);
   this.addMethod("submitCourse", submitCourse);
 });
+
+function createLesson() {}
+
+function createCard() {}
+
+function createWord() {}
+
+function findWord() {}
 
 function submitCourse() {}
