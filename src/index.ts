66c5c37e74143f39bed01237924fc27557048e1e
bctran <bao.cong.tran@mgm-tp.com>
2023-10-22 17:56:09 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,42 @@
 import express from "express";
-import {default as loginJsonRpcMethodHandlers} from "./login/jsonRpcMethodHandlers";
-import {default as adminJsonRpcMethodHandlers} from "./admin/jsonRpcMethodHandlers";
-import {default as courseJsonRpcMethodHandlers} from "./course/jsonRpcMethodHandlers";
+import { default as loginJsonRpcMethodHandlers } from "./login/jsonRpcMethodHandlers";
+import { default as adminJsonRpcMethodHandlers } from "./admin/jsonRpcMethodHandlers";
+import { default as studyJsonRpcMethodHandlers } from "./study/jsonRpcMethodHandlers";
 import { JSONRPCServer } from "json-rpc-2.0";
 import "scope-extensions-js";
 import { processJWT } from "./jwt/jwt";
 
 express()
-  .also(app => {
-      // JSON-RPC is used so the json body must always be parsed
-      app.use(express.json());
+  .also((app) => {
+    // JSON-RPC is used so the json body must always be parsed
+    app.use(express.json());
 
-      // Login
-      app.use("/login", jsonRpcRouter(loginJsonRpcMethodHandlers));
+    // Login
+    app.use("/login", jsonRpcRouter(loginJsonRpcMethodHandlers));
 
-      // JWT token must be verified for following routes
-      app.use(processJWT);
+    // JWT token must be verified for following routes
+    app.use(processJWT);
 
-      app.use("/admin", jsonRpcRouter(adminJsonRpcMethodHandlers));
-      app.use("/course", jsonRpcRouter(courseJsonRpcMethodHandlers));
+    app.use("/admin", jsonRpcRouter(adminJsonRpcMethodHandlers));
+    app.use("/study", jsonRpcRouter(studyJsonRpcMethodHandlers));
   })
-  .listen(3000, () => { console.log("Express server started on port 3000."); });
+  .listen(3000, () => {
+    console.log("Express server started on port 3000.");
+  });
 
 function jsonRpcRouter(jsonRpcMethodHandlers: JSONRPCServer<void>) {
-  return express.Router().let(router => {
+  return express.Router().let((router) => {
     return router.post("/", (request, response) => {
       jsonRpcMethodHandlers.receive(request.body).then((jsonRpcResponse) => {
-          if (jsonRpcResponse) {
-            response.json(jsonRpcResponse);
-          } else {
-            /*
-            * The Server MUST NOT reply to a Notification.
-            * Ref: https://www.jsonrpc.org/specification#notification
-            */
-          }
+        if (jsonRpcResponse) {
+          response.json(jsonRpcResponse);
+        } else {
+          /*
+           * The Server MUST NOT reply to a Notification.
+           * Ref: https://www.jsonrpc.org/specification#notification
+           */
+        }
       });
     });
-  })
+  });
 }
