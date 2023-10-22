39c280cba93686e8a8643429affec16b645dacab
bctran <bao.cong.tran@mgm-tp.com>
2023-10-22 20:02:42 +0700

    Update

--- src/error/error.ts
+++ src/error/error.ts
@@ -2,21 +2,24 @@ import { JSONRPCID, createJSONRPCErrorResponse } from "json-rpc-2.0";
 
 export { sendJsonRpcErrorResponse, JsonRpcErrorCodes };
 
-function sendJsonRpcErrorResponse(response: any, id: JSONRPCID, code: any) {
+function sendJsonRpcErrorResponse(response: any, id: JSONRPCID, code: JsonRpcErrorCodes) {
   response.json(createJSONRPCErrorResponse(id, code, jsonRpcErrorCodes[code]));
 }
 
-const jsonRpcErrorCodes: Record<number, string> = {
-  [-33000]: "Authentication failed: Unexpected error while verifying JWT token.",
-  [-33001]: "Authentication failed: Missing Authorization header.",
-  [-33002]: "Authentication failed: Missing JWT token.",
-  [-33003]: "Authentication failed: Invalid JWT token.",
-};
-
 enum JsonRpcErrorCodes {
+  //Authorization
   Authorization_UnexpectedError = -33000,
   Authorization_AuthorizationHeaderMissing = -33001,
   Authorization_JwtTokenMissing = -33002,
   Authorization_JwtTokenExpired = -33003,
-  Authorization_JwtTokenInvalid = -33003,
+  Authorization_JwtTokenInvalid = -33004,
 }
+
+const jsonRpcErrorCodes: Record<JsonRpcErrorCodes, string> = {
+  //Authorization
+  [JsonRpcErrorCodes.Authorization_UnexpectedError]: "Authorization failed: Unexpected error.",
+  [JsonRpcErrorCodes.Authorization_AuthorizationHeaderMissing]: "Authorization failed: Missing Authorization header.",
+  [JsonRpcErrorCodes.Authorization_JwtTokenMissing]: "Authorization failed: Missing JWT token.",
+  [JsonRpcErrorCodes.Authorization_JwtTokenExpired]: "Authorization failed: JWT token expired.",
+  [JsonRpcErrorCodes.Authorization_JwtTokenInvalid]: "Authorization failed: Invalid JWT token.",
+};
