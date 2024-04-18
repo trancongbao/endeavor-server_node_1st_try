d4fdff2f37f7677c979d962a9b136dd6e1fa0f96
bctran <bao.cong.tran@mgm-tp.com>
2024-04-18 16:03:27 +0700

    docker-compose.yml

---
+++ docker-compose/postgres/docker-entrypoint-initdb.d/docker-entrypoint-initdb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  \connect $POSTGRES_DB $POSTGRES_USER
+  BEGIN;
+    CREATE TABLE IF NOT EXISTS event (
+	  id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+	  aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+	  event_data JSON NOT NULL,
+	  version INT,
+	  UNIQUE(aggregate_id, version)
+	);
+	CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
+  COMMIT;
+EOSQL
