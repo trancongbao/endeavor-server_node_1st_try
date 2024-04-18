8596b6f7128dacbdff12e3251ae4868cd991a931
bctran <bao.cong.tran@mgm-tp.com>
2024-04-18 16:09:51 +0700

    docker-compose.yml

--- docker-compose/postgres/docker-entrypoint-initdb.d/docker-entrypoint-initdb.sh
+++ docker-compose/postgres/docker-entrypoint-initdb.d/docker-entrypoint-initdb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   \connect $POSTGRES_DB $POSTGRES_USER
   BEGIN;
